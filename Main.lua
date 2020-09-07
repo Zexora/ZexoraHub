8fc117dcf135066a2a7497db48a60b40b7eaa508
Zexora <70862191+Zexora@users.noreply.github.com>
2020-09-07 22:10:02 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,3 +3,5 @@ local game = game.PlaceId
 --Games List:
 local p1 = 4893679160 --Big Brain Simulator
 local p2 = 155615604--Prison Life
+
+https://raw.githubusercontent.com/Zexora/ZexoraHub/master/Ui/Loading.lua
