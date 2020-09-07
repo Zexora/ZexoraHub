21b9336b9fb3303ad529743f9c5ad558d5d3aa6f
Zexora <70862191+Zexora@users.noreply.github.com>
2020-09-07 22:11:11 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,4 +4,12 @@ local game = game.PlaceId
 local p1 = 4893679160 --Big Brain Simulator
 local p2 = 155615604--Prison Life
 
-https://raw.githubusercontent.com/Zexora/ZexoraHub/master/Ui/Loading.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zexora/ZexoraHub/master/Ui/Loading.lua"))()
+
+if game == p1 then
+  
+  end
+
+elseif game == p2 then
+
+end
